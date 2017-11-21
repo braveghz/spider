5a60c571a1434d9db4cf58296b511fbada1409b3
braveghz <braveghz@gmail.com>
2017-11-21 21:43:46 +0800

    urlopen+re

---
+++ 1/spider1.py
@@ -0,0 +1,27 @@
+
+# -*- coding=utf-8 -*-
+
+import urllib
+import re
+
+question_url = 'https://www.zhihu.com/question/40007169'
+
+
+def get_img(_url):
+    response = urllib.urlopen(_url)
+    html = response.read()
+
+    reg = r'data-actualsrc="(.+?\.jpg)"'
+    img_re = re.compile(reg)
+    img_list = re.findall(img_re, html)
+
+    for i in range(5):
+        urllib.urlretrieve(img_list[i], '%s.jpg' % i)
+
+
+get_img(question_url)
+
+"""
+urllib.urlretrieve(url[, filename[, reporthook[, data]]])  # 直接将远程数据下载到本地
+正则匹配 需要看一下源代码 决定正则写啥
+"""
